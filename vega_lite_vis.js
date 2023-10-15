ba31fc21c51190c515e1cc4893de0ba1d9b20725
Elee0068 <145523249+Elee0068@users.noreply.github.com>
2023-10-15 08:55:48 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,16 +1,26 @@
 var vg_1 = "GlobalWineProducers.vg.json"; 
 var vg_2 = "TopWineProducers.vg.json";
-var vg_3 = "area.vg.json";
+var vg_3 = "HistWineTrade.vg.json";
+var vg_4 = "GlobalWineExport.vg.json";
+var vg_5 = "GlobalWineImports.vg.json";
 
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#pie", vg_2).then(function(result) {
+vegaEmbed("#pie", vg_2, {"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#area", vg_3).then(function(result) {
+vegaEmbed("#area", vg_3, {"actions": false}).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#extree", vg_4, {"actions": false}).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#imtree", vg_5, {"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
